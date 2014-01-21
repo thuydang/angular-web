8a3b594a1b3f810b8f6cb2556a62096fadb1bda8
Xuan-Thuy Dang <ThuyDang.de@gmail.com>
2014-01-21 13:34:51 +0100

    initial commit

---
+++ js/ui-bootstrap/controllers.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/* UI Controllers */
+
+var phonecatUIControllers = angular.module('phonecatUIControllers', ['ui.bootstrap']);
+
+phonecatUIControllers.controller('UICollapseController', ['$scope', 
+		function ($scope) {
+			$scope.ui = $scope.ui || {};
+			$scope.ui.menu = {
+				isCollapsed:"true",
+				menuItems:[
+					{name:"Phones", url:"phones"},
+					{name:"Car", url:"car"},
+					{name:"Traffic Info", url:"traffic"},
+					{name:"Map", url:"map"}]	
+			};
+
+			//$scope.ui.menu.isCollapsed = true;
+		}]);
+
+phonecatUIControllers.controller('UITabController', ['$scope', 
+		function ($scope) {
+			//$scope.ui = {};
+			$scope.ui = $scope.ui || {};
+			$scope.ui.tabs = [
+				{ title:"Dynamic Title 1", 
+					content:"Dynamic content 1",
+					page:"partials/phone-list.html",
+					alertMe: function() {
+						setTimeout(function() {
+							//alert("You've selected the alert tab!");
+						});
+					}
+				},
+				{ title:"Dynamic Title 3", 
+					content:"Dynamic content 3",
+					page:"partials/phone-detail.html",
+					alertMe: function() {
+						setTimeout(function() {
+							//alert("You've selected the alert tab!");
+						});
+					}
+				},
+	    	{ title:"Dynamic Title 2", 
+					content:"Dynamic content 2", 
+					disabled: true
+				}
+			];
+
+			//$scope.ui.menu.isCollapsed = true;
+		}]);
+
+
+/*
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+  function($scope, Phone) {
+    $scope.phones = Phone.query();
+    $scope.orderProp = 'age';
+  }]);
+*/
+
